b029947e893f5598ff6c2492f98e8e651324e5e3
Alan Ngo <45725511+yykk7711@users.noreply.github.com>
2021-08-23 17:52:59 +0800

    Minor changes done

--- script.js
+++ script.js
@@ -115,3 +115,34 @@ $(document).on("scroll", function() {
     }
   }
 });
+
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag3");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible3");
+    } else {
+      $(tag).removeClass("visible3");
+    }
+  }
+});
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag4");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible4");
+    } else {
+      $(tag).removeClass("visible4");
+    }
+  }
+});
